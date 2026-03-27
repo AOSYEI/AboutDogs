3ee1c168d60ca466505d7ad414d10e06be9e06d2
aosyei <jinbiaoxie72@gmail.com>
2026-03-27 22:19:00 +0800

    chore: add github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+const isUserOrOrgPages = repositoryName?.toLowerCase().endsWith('.github.io');
+
 export default defineConfig({
+  base: isGitHubActions
+    ? repositoryName && !isUserOrOrgPages
+      ? `/${repositoryName}/`
+      : '/'
+    : '/',
   plugins: [react()],
   resolve: {
     alias: {
